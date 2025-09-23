3e25cc44fc9043fd3f27d1a2c32a68cc7c6ce0d8
Saqib <rooott@gmail.com>
2025-09-23 19:15:34 +0500

    moved the API url from .env to apiClient

--- app/frontend/src/api/apiClient.js
+++ app/frontend/src/api/apiClient.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: import.meta.env.VITE_API_BASE_URL || "", // e.g. "http://localhost:5000"
+  baseURL: "http://localhost:4000" || "", // e.g. "http://localhost:5000"
   headers: { "Content-Type": "application/json" },
   // timeout: 10000,
 });
